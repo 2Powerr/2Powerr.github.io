6018d0174069b5b5abac8d5c5870134b45c3bef6
Edgar Silva <116201245+2Powerr@users.noreply.github.com>
2023-02-01 15:50:59 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,32 +1,32 @@
-const INIT_ANIMATION_FILENAME = 'sample_video.webm'
+const INIT_ANIMATION_FILENAME = 'sample_video.webm.webm'
 
 const SOUND_BOXES = [
 	{
 		id: 'box-1',
-		description: 'Description of box 1. This description can be changed in the config.js file',
-		previewSoundFilename: 'preview_box_1.mp3',
-		soundFilename: 'preview_box_1.mp3',
+		description: 'Description of box 1.',
+		previewSoundFilename: 'preview_box_1.wav',
+		soundFilename: 'preview_box_1.wav',
 		previewImageFilename: 'box_1_small.jpg'
 	},
 	{
 		id: 'box-2',
-		description: 'Description of box 2. This description can be changed in the config.js file',
-		previewSoundFilename: 'preview_box_2.mp3',
-		soundFilename: 'preview_box_2.mp3',
+		description: 'Description of box 2.',
+		previewSoundFilename: 'preview_box_2.wav',
+		soundFilename: 'preview_box_2.wav',
 		previewImageFilename: 'box_2_small.jpg'
 	},
 	{
 		id: 'box-3',
-		description: 'Description of box 3. This description can be changed in the config.js file',
-		previewSoundFilename: 'preview_box_3.mp3',
-		soundFilename: 'preview_box_3.mp3',
+		description: 'Description of box 3.',
+		previewSoundFilename: 'preview_box_3.wav',
+		soundFilename: 'preview_box_3.wav',
 		previewImageFilename: 'box_3_small.jpg'
 	},
 	{
 		id: 'box-4',
-		description: 'Description of box 4. This description can be changed in the config.js file',
-		previewSoundFilename: 'preview_box_4.mp3',
-		soundFilename: 'preview_box_4.mp3',
+		description: 'Description of box 4.',
+		previewSoundFilename: 'preview_box_4.wav',
+		soundFilename: 'preview_box_4.wav',
 		previewImageFilename: 'box_4_small.jpg'
 	},
 ]
