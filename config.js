edbbe9449116eb3cad9c5a9753deefbebf393129
Edgar Silva <116201245+2Powerr@users.noreply.github.com>
2023-02-02 12:49:48 +0000

    Update config.js

--- config.js
+++ config.js
@@ -3,28 +3,28 @@ const INIT_ANIMATION_FILENAME = 'sample_video.webm.webm'
 const SOUND_BOXES = [
 	{
 		id: 'box-1',
-		description: 'Café<br><br>Pessoas<br>Musica.',
+		description: 'Café\n\nPessoas\nMusica.',
 		previewSoundFilename: 'preview_box_1.wav',
 		soundFilename: 'preview_box_1.wav',
 		previewImageFilename: 'box_1_small.jpg'
 	},
 	{
 		id: 'box-2',
-		description: 'Parque D.Carlos I<br><br>Pessoas<br>Fauna.',
+		description: 'Parque D.Carlos I\n\nPessoas\nFauna.',
 		previewSoundFilename: 'preview_box_2.wav',
 		soundFilename: 'preview_box_2.wav',
 		previewImageFilename: 'box_2_small.jpg'
 	},
 	{
 		id: 'box-3',
-		description: 'Skatepark<br><br>Trânsito<br>Skate.',
+		description: 'Skatepark\n\nTrânsito\nSkate.',
 		previewSoundFilename: 'preview_box_3.wav',
 		soundFilename: 'preview_box_3.wav',
 		previewImageFilename: 'box_3_small.jpg'
 	},
 	{
 		id: 'box-4',
-		description: 'Praça da fruta<br><br>Pessoas<br>Musica.',
+		description: 'Praça da fruta\n\nPessoas\nMusica.',
 		previewSoundFilename: 'preview_box_4.wav',
 		soundFilename: 'preview_box_4.wav',
 		previewImageFilename: 'box_4_small.jpg'
