999f2854aec9630594675bdb9e5ce76be23bde42
Edgar Silva <116201245+2Powerr@users.noreply.github.com>
2023-02-02 12:47:21 +0000

    Update config.js

--- config.js
+++ config.js
@@ -10,21 +10,21 @@ const SOUND_BOXES = [
 	},
 	{
 		id: 'box-2',
-		description: 'Description of box 2.',
+		description: 'Parque D.Carlos I<br><br>Pessoas<br>Fauna.',
 		previewSoundFilename: 'preview_box_2.wav',
 		soundFilename: 'preview_box_2.wav',
 		previewImageFilename: 'box_2_small.jpg'
 	},
 	{
 		id: 'box-3',
-		description: 'Description of box 3.',
+		description: 'Skatepark<br><br>Trânsito<br>Skate.',
 		previewSoundFilename: 'preview_box_3.wav',
 		soundFilename: 'preview_box_3.wav',
 		previewImageFilename: 'box_3_small.jpg'
 	},
 	{
 		id: 'box-4',
-		description: 'Description of box 4.',
+		description: 'Praça da fruta<br><br>Pessoas<br>Musica.',
 		previewSoundFilename: 'preview_box_4.wav',
 		soundFilename: 'preview_box_4.wav',
 		previewImageFilename: 'box_4_small.jpg'
